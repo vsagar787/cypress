087de8a075d6f65e495e6d6d2e563bf72bc90775
SAGAR GUDETI <vsagar787@gmail.com>
2023-03-06 23:06:39 +0530

    Update test.js

--- test.js
+++ test.js
@@ -4,3 +4,13 @@ Assumptions:
 2. It can be integrated with any CI tool equipped for headless execution with command-line options.
 3. It captures screenshots of test execution automatically in case of test failure for effective debugging.
 4. Automatically reloads Test Runner once the changes have been made to the test.
+
+
+Conclusion:
+
+Cypress is a powerful testing framework that can help improve the quality of web applications by automating tests and making it easier to catch and fix issues before they reach production.
+
+1. It is a next-generation UI automation tool, which executes the test cases directly inside the browser.
+2. Additionally, it provides the inbuilt test runner, using which a user can run the test cases directly from Cypress UI.
+3. It provides a Dashboard service, which displays the results of all test runs.
+4. It provides various unique features like Time Travel, Automatic wait, Stubs, etc. which makes it unique among the various other UI automation tools.
