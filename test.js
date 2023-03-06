f6b60f3c457b689fa827103036d692abae030821
SAGAR GUDETI <vsagar787@gmail.com>
2023-03-06 22:59:12 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,20 +1,6 @@
-Cypress is a JavaScript-based end-to-end testing framework that is used to automate tests for web applications. It allows developers and QA engineers to write and execute automated tests directly in the browser, making it easy to simulate user actions and interactions with web applications.
-
-Some key features of the Cypress framework include:
-
-1. Automatic waiting: Cypress automatically waits for elements to appear and become interactive before executing commands, which eliminates the need for explicit waits.
-
-2. Time-travel debugging: Cypress allows developers to debug their tests by stepping through each command in real-time, which can help identify issues faster.
-
-3. Built-in assertions: Cypress comes with built-in assertions that make it easy to verify the state of the application under test.
-
-4. Easy setup: Cypress is easy to set up and requires minimal configuration, making it a popular choice for developers and QA engineers.
-
-Overall, Cypress is a powerful testing framework that can help improve the quality of web applications by automating tests and making it easier to catch and fix issues before they reach production.
-
-
-
-
-
-link
-https://www.browserstack.com/guide/cypress-framework-tutorial
+Problem Statement : Advantages of Cypress Web Testing
+Assumptions:
+1. this should execute automated tests directly in the browser
+2. It can be integrated with any CI tool equipped for headless execution with command-line options.
+3. It captures screenshots of test execution automatically in case of test failure for effective debugging.
+4. Automatically reloads Test Runner once the changes have been made to the test.
